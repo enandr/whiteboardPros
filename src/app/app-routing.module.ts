0d28f7e227117753a2b57087fd5acd90fdd7ad71
Roger Enand <rogerenand@gmail.com>
2020-10-28 00:24:05 -0700

    added footer added content to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+import { HomeComponent } from './Pages/home/home.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
+
+//primng
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FooterComponent } from './Theme/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //primeng
+    ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
